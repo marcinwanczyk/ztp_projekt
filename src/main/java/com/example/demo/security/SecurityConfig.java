8847ec1187923dc7a549f4ac51bb100ffbda0bd5
00Flash <anonimowyflash@wp.planonimowyflash@wp.pl>
2025-01-05 23:30:01 +0100

    Basic security

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        String[] allowedPaths = { "/", "/login", "/logout", "/register" };
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(allowedPaths).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/?logout")
+                );
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
